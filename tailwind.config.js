d72f286802632b9f67fd4b37c88c20806bbcc52f
Raki <killer.vp.mic@gmail.com>
2023-03-19 17:53:30 -0300

    tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'not-quite-black': '#252525',
+        'logo-blue': '#3b5c99',
+      }
+    },
+  },
+  plugins: [],
+}
