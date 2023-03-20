2ec86b336f7e6dd4776880ab73639387ad6b5388
Raki <killer.vp.mic@gmail.com>
2023-03-19 21:46:12 -0300

    fixed header positioning, now sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import HeaderCombobox from './HeaderCombobox';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -11,8 +11,29 @@ export interface PostType {
 }
 
 const Header: FC<PostType> = ({ postData }) => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+  
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const visible = prevScrollPos > currentScrollPos;
+  
+      setPrevScrollPos(currentScrollPos);
+      setVisible(visible);
+    };
+  
+    React.useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [prevScrollPos, visible, handleScroll]);
+
     return (
-        <header className="h-16 border-b-[1px] border-gray-400">
+        <header className={`h-16 bg-white border-b-[1px] border-gray-400 fixed top-0 w-full z-50 transition-all duration-300 ${
+            visible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+          }`}>
             <div className="flex justify-between xl:max-w-7xl xl:mx-auto">
                     <Link href="/" className="flex items-center transition-all delay-75 duration-200 hover:scale-110">
                         <Image src="/svg/mainLogo.svg" width={35} height={35} alt="ademondev logo"
