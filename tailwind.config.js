6dee0c3a00ee9e1b3f3d46188cb665c02495942d
Raki <killer.vp.mic@gmail.com>
2023-03-24 19:00:34 -0300

    added frontmatter config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
       colors: {
         'not-quite-black': '#252525',
         'logo-blue': '#3b5c99',
+      },
+      backgroundImage: {
+        'repeat': "url('/svg/repeat.svg')",
+      },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': {transform: 'translateX(-50%)'},
+        },
+      },
+      animation: {
+        wave: 'wave linear 7s infinite',
       }
     },
   },
